bba2604ed2da471ecb17ace5e95718cc6477fbca
Utoog <sekreroy7@gmail.com>
2024-10-02 17:57:21 +0700

    fixed fetching posts file from remote directory

--- main.py
+++ main.py
@@ -14,15 +14,16 @@ neocities_profile_name = ""
 posts_name = "posts"
 posts_filename = posts_name + ".json"
 
+# Where your post file will be saved on your neocities page (DEFAULT: "")
+web_post_directory = ""
+
 nc = neocities.NeoCities(api_key=neocities_api_key)
 
 # TODO: add a check if remote file exists
-urllib.request.urlretrieve("https://" + neocities_profile_name + "/" + posts_filename, posts_filename)
+urllib.request.urlretrieve("https://" + neocities_profile_name + "/" + web_post_directory + posts_filename, posts_filename)
 
+# How do you want your date displayed on your posts (Default: "%d.%m.%Y %H:%M" = "20.1.2000 12:00")
 format_string = "%d.%m.%Y %H:%M"
-
-# Where your post file will be saved on your neocities page (DEFAULT: "")
-web_post_directory = ""
 
 def get_current_timedate():
     return str(datetime.datetime.now().strftime(format_string))
